b565ee0761d3fd38939ece320f2e500116d33a26
Lucaszioli <lucas.zioli.04@gmail.com>
2023-10-04 16:40:27 -0300

    feat: lê os dados de uma csv

---
+++ Model/Model.ts
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import csv from 'csv-parser'
+
+interface Data {
+    title: string;
+    value: string;
+  }
+//Lendo os dados
+const readCSV = async (filePath: string) : Promise<Data[]> => {
+    return new Promise ((resolve, reject) => {
+        const results: Data[] = [];
+        fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data : Data)=>results.push(data))
+        .on('end', () => resolve(results))
+        .on('error', (error) => reject(error));
+    });
+};
